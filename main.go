e97e9e8a07b3f3b504e104e2f73a49747cb66d14
yczha <yooongchun@foxmail.com>
2023-05-20 13:06:46 +0800

    feat: add config framework

--- main.go
+++ main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gohub/bootstrap"
+	btsConfig "gohub/config"
+	"gohub/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	btsConfig.Initialize()
+}
 func main() {
+	// load env
+	var env string
+	flag.StringVar(&env, "env", "", "load .env file, using .env.testing when \"--env=testing\" used")
+	flag.Parse()
+	config.InitConfig(env)
+
 	//init gin instance
 	router := gin.New()
 
 	bootstrap.SetupRoute(router)
 
-	err := router.Run(":8080")
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		fmt.Println(err.Error())
 	}

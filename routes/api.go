cec2a1f6c0e956210ba0fc8dcc7723e30895dfec
yooongchun <yooongchun@foxmail.com>
2023-05-20 21:41:05 +0800

    feat: add captcha route

--- routes/api.go
+++ routes/api.go
@@ -20,6 +20,10 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
 			// is email registered?
 			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
+			// 发送验证码
+			vcc := new(auth.VerifyCodeController)
+			// 图片验证码，需要加限流
+			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
 		}
 	}
 }

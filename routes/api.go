da948e3d759ea7299903b8d2588795de80239f19
yooongchun <yooongchun@foxmail.com>
2023-05-21 08:34:39 +0800

    feat: use phone to register user

--- routes/api.go
+++ routes/api.go
@@ -26,6 +26,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
 			authGroup.POST("/verify-codes/phone", vcc.SendUsingPhone)
 			authGroup.POST("/verify-codes/email", vcc.SendUsingEmail)
+			authGroup.POST("/signup/using-phone", suc.SignupUsingPhone)
 		}
 	}
 }

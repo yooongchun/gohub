d3e221bd03984d4d83f3c52ab5f26c175c5da9ef
yooongchun <yooongchun@foxmail.com>
2023-05-21 13:44:10 +0800

    feat: add command line module

--- main.go
+++ main.go
@@ -1,45 +1,62 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"gohub/app/cmd"
 	"gohub/bootstrap"
-	btsConfig "gohub/config"
+	btsConig "gohub/config"
 	"gohub/pkg/config"
+	"gohub/pkg/console"
+	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/spf13/cobra"
 )
 
 func init() {
-	btsConfig.Initialize()
+	// 加载 config 目录下的配置信息
+	btsConig.Initialize()
 }
+
 func main() {
-	// load env
-	var env string
-	flag.StringVar(&env, "env", "", "load .env file, using .env.testing when \"--env=testing\" used")
-	flag.Parse()
-	config.InitConfig(env)
 
-	// 初始化 Logger
-	bootstrap.SetupLogger()
+	// 应用的主入口，默认调用 cmd.CmdServe 命令
+	var rootCmd = &cobra.Command{
+		Use:   "Gohub",
+		Short: "A simple forum project",
+		Long:  `Default will run "serve" command, you can use "-h" flag to see all subcommands`,
 
-	// 设置 gin 的运行模式，支持 debug, release, test
-	// release 会屏蔽调试信息，官方建议生产环境中使用
-	// 非 release 模式 gin 终端打印太多信息，干扰到我们程序中的 Log
-	// 故此设置为 release，有特殊情况手动改为 debug 即可
-	gin.SetMode(gin.ReleaseMode)
+		// rootCmd 的所有子命令都会执行以下代码
+		PersistentPreRun: func(command *cobra.Command, args []string) {
 
-	//init gin instance
-	router := gin.New()
+			// 配置初始化，依赖命令行 --env 参数
+			config.InitConfig(cmd.Env)
 
-	// 初始化 DB
-	bootstrap.SetupDB()
-	bootstrap.SetupRedis()
-	// 初始化路由
-	bootstrap.SetupRoute(router)
+			// 初始化 Logger
+			bootstrap.SetupLogger()
 
-	err := router.Run(":" + config.Get("app.port"))
-	if err != nil {
-		fmt.Println(err.Error())
+			// 初始化数据库
+			bootstrap.SetupDB()
+
+			// 初始化 Redis
+			bootstrap.SetupRedis()
+
+			// 初始化缓存
+		},
+	}
+
+	// 注册子命令
+	rootCmd.AddCommand(
+		cmd.CmdServe,
+	)
+
+	// 配置默认运行 Web 服务
+	cmd.RegisterDefaultCmd(rootCmd, cmd.CmdServe)
+
+	// 注册全局参数，--env
+	cmd.RegisterGlobalFlags(rootCmd)
+
+	// 执行主命令
+	if err := rootCmd.Execute(); err != nil {
+		console.Exit(fmt.Sprintf("Failed to run app with %v: %s", os.Args, err.Error()))
 	}
 }

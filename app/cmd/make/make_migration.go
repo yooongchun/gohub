9bebb17829d57a5790776b44b16044863d04f339
yooongchun <yooongchun@foxmail.com>
2023-05-22 21:34:08 +0800

    feat: add "make migration command"

--- app/cmd/make/make_migration.go
+++ app/cmd/make/make_migration.go
@@ -16,7 +16,6 @@ var CmdMakeMigration = &cobra.Command{
 }
 
 func runMakeMigration(cmd *cobra.Command, args []string) {
-
 	// 日期格式化
 	timeStr := app.TimenowInTimezone().Format("2006_01_02_150405")
 
